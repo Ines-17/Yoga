2b5dd58929c9ea30694f185f40f602e22bbc0458
Ines-17 <75087033+Ines-17@users.noreply.github.com>
2021-02-15 15:28:03 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+const mobileBtn = document.getElementById('mobile-cta')
+   nav = document.querySelector('nav')
+   mobileBtnExit = document.getElementById('mobile-exit');
+
+mobileBtn.addEventListener('click', function(){
+nav.classList.add('menu-btn');
+})
+
+mobileBtnExit.addEventListener('click', function() {
+  nav.classList.remove('menu-btn');
+})
